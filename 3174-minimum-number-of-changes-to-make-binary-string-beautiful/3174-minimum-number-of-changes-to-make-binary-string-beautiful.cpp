a7e8c0fb20d6534af81e730ac80711b6d4ce812f
Victor Hugo <78633759+vector2357@users.noreply.github.com>
2024-11-05 00:41:26 -0300

    Time: 0 ms (100%), Space: 12 MB (96.18%) - LeetHub

---
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minChanges(string s) {
+        int cont = 1, ans = 0;
+
+        for(int i=1; i<s.size(); i++) {
+            if(s[i] == s[i-1]) {
+                cont++;
+            }
+            else {
+                if(cont%2) {
+                    ans++;
+                    cont = 0;
+                }
+                else {
+                    cont = 1;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
