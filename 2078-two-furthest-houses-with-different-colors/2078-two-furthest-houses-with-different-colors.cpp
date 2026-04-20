793e591d4495a336993aa19fb370284bd298db44
Victor Hugo <78633759+vector2357@users.noreply.github.com>
2026-04-20 00:11:05 -0300

    Time: 0 ms (100%), Space: 12.2 MB (40.24%) - LeetHub

---
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+        int ans = 0;
+        for (int i=0; i<n-1; i++) {
+            if (colors[i] != colors[n-1]) {
+                ans = n-1-i;
+                break;
+            }
+        }
+        for (int i=n-1; i>0; i--) {
+            if (colors[i] != colors[0]) {
+                ans = max(ans, i);
+                break;
+            }
+        }
+        return ans;
+    }
+};
